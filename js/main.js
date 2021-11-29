0fd396624232c31ec8161845b53956610b461060
HigherLight <higherlightstudios@gmail.com>
2021-11-29 21:22:58 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function tooltip() {
+    if (document.getElementById("tooltip").style.opacity == 0) {
+        document.getElementById("tooltip").style.opacity = 1;
+    } else {
+        document.getElementById("tooltip").style.opacity = 0;
+    }
+}
